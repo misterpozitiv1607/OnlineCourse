64afb0570486c8c4c36b62172487fddfdc47bb96
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-10 19:52:59 +0500

    file deleted

--- OnlineCourse.DAL/IRepositories/Courses/ICourseCategoryRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using OnlineCourse.DAL.IRepositories.GenericRepository;
-using OnlineCourse.Domain.Entities.CourseCategories;
-
-namespace OnlineCourse.DAL.IRepositories.Courses;
-
-public interface ICourseCategoryRepository : IRepository<CourseCategory>
-{
-
-}
--- OnlineCourse.DAL/IRepositories/Courses/ICourseRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using OnlineCourse.DAL.IRepositories.GenericRepository;
-using OnlineCourse.Domain.Entities.Courses;
-
-namespace OnlineCourse.DAL.IRepositories.Courses;
-
-public interface ICourseRepository : IRepository<Course>
-{
-
-}
