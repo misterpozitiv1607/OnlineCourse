ee89a60960e834d15957020a108aae7d960609e4
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-10 19:54:35 +0500

    delete files

--- OnlineCourse.DAL/IRepositories/Orders/IOrderRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using OnlineCourse.DAL.IRepositories.GenericRepository;
-using OnlineCourse.Domain.Entities.Orders;
-
-namespace OnlineCourse.DAL.IRepositories.Orders;
-
-public interface IOrderRepository : IRepository<Order>
-{
-
-}
--- OnlineCourse.DAL/IRepositories/Students/IStudentRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using OnlineCourse.DAL.IRepositories.GenericRepository;
-using OnlineCourse.Domain.Entities.Students;
-
-namespace OnlineCourse.DAL.IRepositories.Students;
-
-public interface IStudentRepository : IRepository<Student>
-{
-
-}
