30ca4ada33220ac4e52ffe633edfac215dfef77c
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-10 17:57:23 +0500

    Finally the DTO is over :)

---
+++ OnlineCourse.Service/Dtos/Courses/CourseResultDto.cs
@@ -0,0 +1,16 @@
+﻿using OnlineCourse.Service.Dtos.Orders;
+using OnlineCourse.Service.Dtos.Students;
+using OnlineCourse.Service.Dtos.Teachers;
+
+namespace OnlineCourse.Service.Dtos.Courses;
+
+public class CourseResultDto
+{
+    public long Id { get; set; }
+    public double Price { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public StudentResultDto StudentResultDto { get; set; }
+    public TeacherResultDto TeacherResultDto { get; set; }
+    public OrderResultDto OrderResultDto { get; set; }
+}
---
+++ OnlineCourse.Service/Dtos/Courses/CourseUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineCourse.Service.Dtos.Courses;
+
+public class CourseUpdateDto
+{
+    public long Id { get; set; }
+    public double Price { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public long StudentId { get; set; }
+    public long TeacherId { get; set; }
+    public long OrderId { get; set; }
+}
