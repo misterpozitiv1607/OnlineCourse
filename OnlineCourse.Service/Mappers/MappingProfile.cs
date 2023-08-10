90e9530e107bbf401ae2859519cd9ce52c88e2e4
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-10 19:02:32 +0500

    Database classs library add IRepository

--- OnlineCourse.Service/Mappers/MappingProfile.cs
+++ OnlineCourse.Service/Mappers/MappingProfile.cs
@@ -1,7 +1,14 @@
 ﻿using AutoMapper;
+using OnlineCourse.Domain.Entities.CourseCategories;
 using OnlineCourse.Domain.Entities.Courses;
+using OnlineCourse.Domain.Entities.Orders;
+using OnlineCourse.Domain.Entities.Students;
+using OnlineCourse.Domain.Entities.Teachers;
 using OnlineCourse.Service.Dtos.CourseCategories;
 using OnlineCourse.Service.Dtos.Courses;
+using OnlineCourse.Service.Dtos.Orders;
+using OnlineCourse.Service.Dtos.Students;
+using OnlineCourse.Service.Dtos.Teachers;
 
 namespace OnlineCourse.Service.Mappers;
 
@@ -9,26 +16,29 @@ public class MappingProfile:Profile
 {
     public MappingProfile()
     {
-        //Course
+        // Course
         CreateMap<Course,CourseCreationDto>().ReverseMap();
         CreateMap<Course, CourseUpdateDto>().ReverseMap();
         CreateMap<Course, CourseResultDto>().ReverseMap();
 
-        //Course
-        CreateMap<Course, CourseCreationDto>().ReverseMap();
-        CreateMap<Course, CourseUpdateDto>().ReverseMap();
-        CreateMap<Course, CourseResultDto>().ReverseMap();
+        // CourseCategory
+        CreateMap<CourseCategory, CourseCategoryCreationDto>().ReverseMap();
+        CreateMap<CourseCategory, CourseCategoryUpdateDto>().ReverseMap();
+        CreateMap<CourseCategory, CourseCategoryResultDto>().ReverseMap();
 
-        //Course
-        CreateMap<Course, CourseCreationDto>().ReverseMap();
-        CreateMap<Course, CourseUpdateDto>().ReverseMap();
-        CreateMap<Course, CourseResultDto>().ReverseMap();
+        // Teacher
+        CreateMap<Teacher, TeacherCreationDto>().ReverseMap();
+        CreateMap<Teacher, TeacherUpdateDto>().ReverseMap();
+        CreateMap<Teacher, TeacherResultDto>().ReverseMap();
 
-        //Course
-        CreateMap<Course, CourseCreationDto>().ReverseMap();
-        CreateMap<Course, CourseUpdateDto>().ReverseMap();
-        CreateMap<Course, CourseResultDto>().ReverseMap();
+        // Student
+        CreateMap<Student, StudentCreationDto>().ReverseMap();
+        CreateMap<Student, StudentUpdateDto>().ReverseMap();
+        CreateMap<Student, StudentResultDto>().ReverseMap();
 
-
+        // Order
+        CreateMap<Order, OrderCreationDto>().ReverseMap();
+        CreateMap<Order, OrderUpdateDto>().ReverseMap();
+        CreateMap<Order, OrderResultDto>().ReverseMap();
     }
 }
--- OnlineCourse.Service/Services/StudentService.cs
+++ OnlineCourse.Service/Services/StudentService.cs
@@ -1,8 +1,38 @@
-﻿using OnlineCourse.Service.Interfaces;
+﻿using OnlineCourse.Service.Dtos.Students;
+using OnlineCourse.Service.Helpers;
+using OnlineCourse.Service.Interfaces;
 
 namespace OnlineCourse.Service.Services;
 
-public class StudentService:IStudentService
+public class StudentService : IStudentService
 {
+    public StudentService()
+    {
+        
+    }
 
+    public Task<Response<StudentResultDto>> CreateAsync(StudentCreationDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Response<bool>> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Response<List<StudentResultDto>>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Response<StudentResultDto>> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Response<StudentResultDto>> UpdateAsync(StudentUpdateDto dto)
+    {
+        throw new NotImplementedException();
+    }
 }
