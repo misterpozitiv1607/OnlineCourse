6f2953f48e71f31b46d82baea05ecc638f9a1c96
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-10 18:21:45 +0500

    Write code for the CourseCategoryinterface

--- OnlineCourse.Service/Interfaces/ICourseCategoryService.cs
+++ OnlineCourse.Service/Interfaces/ICourseCategoryService.cs
@@ -1,6 +1,14 @@
-﻿namespace OnlineCourse.Service.Interfaces;
+﻿using OnlineCourse.Service.Dtos.CourseCategories;
+using OnlineCourse.Service.Dtos.Courses;
+using OnlineCourse.Service.Helpers;
+
+namespace OnlineCourse.Service.Interfaces;
 
 public interface ICourseCategoryService
 {
-
+    Task<Response<CourseCategoryResultDto>> CreateAsync(CourseCategoryCreationDto dto);
+    Task<Response<CourseCategoryResultDto>> UpdateAsync(CourseCategoryUpdateDto dto);
+    Task<Response<bool>> DeleteAsync(long id);
+    Task<Response<CourseCategoryResultDto>> GetByIdAsync(long id);
+    Task<Response<List<CourseCategoryResultDto>>> GetAllAsync();
 }
