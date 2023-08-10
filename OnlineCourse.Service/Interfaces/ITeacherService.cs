a2b0c56813657994f3a6460c1053783564281679
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-10 18:10:40 +0500

    Write code for the teacher interface

--- OnlineCourse.Service/Interfaces/ITeacherService.cs
+++ OnlineCourse.Service/Interfaces/ITeacherService.cs
@@ -1,5 +1,13 @@
-﻿namespace OnlineCourse.Service.Interfaces;
+﻿using OnlineCourse.Service.Dtos.Teachers;
+using OnlineCourse.Service.Helpers;
+
+namespace OnlineCourse.Service.Interfaces;
 
 public interface ITeacherService
 {
+    Task<Response<TeacherResultDto>> CreateAsync(TeacherCreationDto dto);
+    Task<Response<TeacherResultDto>> UpdateAsync(TeacherUpdateDto dto);
+    Task<Response<bool>> DeleteAsync(long id);
+    Task<Response<TeacherResultDto>> GetByIdAsync(long id);
+    Task<Response<List<TeacherResultDto>>> GetAllAsync();
 }
