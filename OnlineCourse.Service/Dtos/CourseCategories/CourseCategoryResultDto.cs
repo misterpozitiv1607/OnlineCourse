6cab7a99772a3a8868fa7b7e9ab3d5d05ce7cbb3
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-10 17:27:40 +0500

    CourseCategory add:CreationDto,UpdateDto and Result dto

--- OnlineCourse.Service/Dtos/CourseCategories/CourseCategoryResultDto.cs
+++ OnlineCourse.Service/Dtos/CourseCategories/CourseCategoryResultDto.cs
@@ -1,7 +1,8 @@
 ﻿namespace OnlineCourse.Service.Dtos.CourseCategories;
 
-public class CourseCategoryCreationDto
+public class CourseCategoryResultDto
 {
+    public long id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public long CourseId { get; set; }
