ac68c081bcd3a65b2d28aa7be5832f4d69a95a4e
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-10 18:52:14 +0500

    Mapping Service add medels

--- OnlineCourse.Service/Mappers/MappingProfile.cs
+++ OnlineCourse.Service/Mappers/MappingProfile.cs
@@ -1,6 +1,34 @@
-﻿namespace OnlineCourse.Service.Mappers;
+﻿using AutoMapper;
+using OnlineCourse.Domain.Entities.Courses;
+using OnlineCourse.Service.Dtos.CourseCategories;
+using OnlineCourse.Service.Dtos.Courses;
 
-public class MappingProfile
+namespace OnlineCourse.Service.Mappers;
+
+public class MappingProfile:Profile
 {
+    public MappingProfile()
+    {
+        //Course
+        CreateMap<Course,CourseCreationDto>().ReverseMap();
+        CreateMap<Course, CourseUpdateDto>().ReverseMap();
+        CreateMap<Course, CourseResultDto>().ReverseMap();
 
+        //Course
+        CreateMap<Course, CourseCreationDto>().ReverseMap();
+        CreateMap<Course, CourseUpdateDto>().ReverseMap();
+        CreateMap<Course, CourseResultDto>().ReverseMap();
+
+        //Course
+        CreateMap<Course, CourseCreationDto>().ReverseMap();
+        CreateMap<Course, CourseUpdateDto>().ReverseMap();
+        CreateMap<Course, CourseResultDto>().ReverseMap();
+
+        //Course
+        CreateMap<Course, CourseCreationDto>().ReverseMap();
+        CreateMap<Course, CourseUpdateDto>().ReverseMap();
+        CreateMap<Course, CourseResultDto>().ReverseMap();
+
+
+    }
 }
