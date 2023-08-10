858bc0713a7c1cf2c5df3402ca14acfa4bf1b188
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-10 18:19:57 +0500

    Write code for the Course interface

--- OnlineCourse.Service/Interfaces/ICourseService.cs
+++ OnlineCourse.Service/Interfaces/ICourseService.cs
@@ -1,5 +1,14 @@
-﻿namespace OnlineCourse.Service.Interfaces;
+﻿using OnlineCourse.Service.Dtos.Courses;
+using OnlineCourse.Service.Dtos.Orders;
+using OnlineCourse.Service.Helpers;
+
+namespace OnlineCourse.Service.Interfaces;
 
 public interface ICourseService
 {
+    Task<Response<CourseResultDto>> CreateAsync(CourseCreationDto dto);
+    Task<Response<CourseResultDto>> UpdateAsync(CourseUpdateDto dto);
+    Task<Response<bool>> DeleteAsync(long id);
+    Task<Response<CourseResultDto>> GetByIdAsync(long id);
+    Task<Response<List<CourseResultDto>>> GetAllAsync();
 }
