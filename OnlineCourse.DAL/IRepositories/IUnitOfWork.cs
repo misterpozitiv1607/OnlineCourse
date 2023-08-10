9cddb5a06ed01d16f5f446db67e4d97673497c62
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-10 19:59:38 +0500

    Change IUnitOfWork

--- OnlineCourse.DAL/IRepositories/IUnitOfWork.cs
+++ OnlineCourse.DAL/IRepositories/IUnitOfWork.cs
@@ -1,6 +1,17 @@
-﻿namespace OnlineCourse.DAL.IRepositories;
+﻿using OnlineCourse.Domain.Entities.CourseCategories;
+using OnlineCourse.Domain.Entities.Courses;
+using OnlineCourse.Domain.Entities.Orders;
+using OnlineCourse.Domain.Entities.Students;
+using OnlineCourse.Domain.Entities.Teachers;
+
+namespace OnlineCourse.DAL.IRepositories;
 
 public interface IUnitOfWork
 {
-
+    IRepository<Student> StudentRepository { get; }
+    IRepository<Teacher> TeacherRepository { get; }
+    IRepository<Course> CourseRepository { get; }
+    IRepository<CourseCategory> CourseCategoryRepository { get; }
+    IRepository<Order> OrderRepository { get; }
+    Task SaveAsync();
 }
