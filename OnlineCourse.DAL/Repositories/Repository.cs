7c8152fdb23df72ce40493c0c7d1a1cd98f37cbe
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-10 20:16:59 +0500

    Change  Repository

--- OnlineCourse.DAL/Repositories/Repository.cs
+++ OnlineCourse.DAL/Repositories/Repository.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineCourse.DAL.IRepositories;
+using OnlineCourse.Domain.Commons;
+using OnlineCourse.DAL.Contexts;
+namespace OnlineCourse.DAL.Repositories;
 
-namespace OnlineCourse.DAL.Repositories
+public class Repository<T> : IRepository<T> where T : Auditable
 {
-    internal class Repository
+    private readonly AppDbContext appDbContex;
+    public Repository()
     {
+        this.appDbContex = new AppDbContext();
     }
+    public async Task CreateAsync(T entity)
+    {
+        await this.appDbContex.Set<T>().AddAsync(entity);
+    }
+
+    public void Update(T entity)
+    {
+        this.appDbContex.Entry(entity).State = EntityState.Modified;
+    }
+
+    public void Delete(T entity)
+    {
+        this.appDbContex.Set<T>().Remove(entity);
+    }
+
+    public async Task<T> SelectById(long id)
+       => await this.appDbContex.Set<T>().FirstOrDefaultAsync(x => x.Id.Equals(id));
+
+    public IQueryable<T> SelectAll()
+       => this.appDbContex.Set<T>().AsNoTracking().AsQueryable();
 }
