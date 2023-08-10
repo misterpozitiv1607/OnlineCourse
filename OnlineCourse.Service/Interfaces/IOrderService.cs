0caae2817c22a39c406ae9ada5f5424f00172152
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-10 18:17:50 +0500

    Write code for the order interface

--- OnlineCourse.Service/Interfaces/IOrderService.cs
+++ OnlineCourse.Service/Interfaces/IOrderService.cs
@@ -1,5 +1,15 @@
-﻿namespace OnlineCourse.Service.Interfaces;
+﻿using System.ComponentModel;
+using OnlineCourse.Service.Dtos.Orders;
+using OnlineCourse.Service.Dtos.Students;
+using OnlineCourse.Service.Helpers;
+
+namespace OnlineCourse.Service.Interfaces;
 
 public interface IOrderService
 {
+    Task<Response<OrderResultDto>> CreateAsync(OrderCreationDto dto);
+    Task<Response<OrderResultDto>> UpdateAsync(OrderUpdateDto dto);
+    Task<Response<bool>> DeleteAsync(long id);
+    Task<Response<OrderResultDto>> GetByIdAsync(long id);
+    Task<Response<List<OrderResultDto>>> GetAllAsync();
 }
