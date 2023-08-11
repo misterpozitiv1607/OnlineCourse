e54b0687d09e69d21e81c8db64240abcf97fe3ca
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-11 11:49:03 +0500

    For Teacher Test Exam

--- OnlineCourse/Views/Teachers/TeacherServiceView.cs
+++ OnlineCourse/Views/Teachers/TeacherServiceView.cs
@@ -8,40 +8,98 @@ namespace OnlineCourse.Views.Teachers
         private TeacherService teacherService = new();
         public async void Create()
         {
-            Console.WriteLine("FirstName: ");
-            string first_name = Console.ReadLine();
+            int count = 0;
+            Console.WriteLine(@"1.Class va Struct Farqi nimada?
+A.Biri Reference tip, biri value
+B.Farqi yo'q
+C.Struct optimalroq classga nisbatan");
+            Console.WriteLine("Quyidagilardan birini tanlang(A,B,C): ");
+            string choice = Console.ReadLine();
+            if (choice == "A" || choice == "a")
+                count += 1;
 
-            Console.WriteLine("LastName: ");
-            string last_name = Console.ReadLine();
+            Console.WriteLine(@"2.int a=8, int b=3 a/b=?
+A.2,6
+B.Xatolik
+C.2");
+            Console.WriteLine("Quyidagilardan birini tanlang(A,B,C): ");
+            string choice1 = Console.ReadLine();
+            if (choice1 == "C" || choice1 =="c")
+                count += 1;
 
-            Console.WriteLine("Date Of Birth(example:yyyy-mm-dd)");
-            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine(@"3Asynxron yoki syxron qaysi biri tezroq?
+A.Syxron
+B.Farqi yo'q
+C.Asynxron");
+            Console.WriteLine("Quyidagilardan birini tanlang(A,B,C): ");
+            string choice2 = Console.ReadLine();
+            if (choice2 == "C" || choice2=="c")
+                count += 1;
 
-            Console.WriteLine("Phone(example:+998xxxxxxxxx)");
-            string phone = Console.ReadLine();
+            Console.WriteLine(@"4.Float o'ziga qanday qiymatlar oladi?
+A.Butun
+B.Xaqiqiy
+C.Istalgan");
+            Console.WriteLine("Quyidagilardan birini tanlang(A,B,C): ");
+            string choice3 = Console.ReadLine();
+            if (choice3 == "B" || choice3=="b")
+                count += 1;
 
-            Console.WriteLine("Email(example:example@gmail.com): ");
-            string email = Console.ReadLine();
+            Console.WriteLine(@"5.Generic tezroqmi yoki non-Generic?
+A.Generic
+B.Farqi yo'q
+C.Nono-Generic");
+            Console.WriteLine("Quyidagilardan birini tanlang(A,B,C): ");
+            string choice4 = Console.ReadLine();
+            if (choice4 == "A" || choice4 == "a")
+                count += 1;
 
-            Console.WriteLine("Experience:(number enter): ");
-            int experience = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Specialty(example:.Net): ");
-            string specialty = Console.ReadLine();
 
-            var result = await teacherService.CreateAsync(new TeacherCreationDto
+
+
+            if (count == 5)
             {
-                FirstName = first_name,
-                LastName = last_name,
-                DateOfBirth = date,
-                Phone = phone,
-                Email = email,
-                Experince = experience,
-                Speciality = specialty
-            });
-            
-            if (result.StatusCode == 200)
-                Console.WriteLine(result.Data.FirstName);
+                Console.WriteLine("FirstName: ");
+                string first_name = Console.ReadLine();
+
+                Console.WriteLine("LastName: ");
+                string last_name = Console.ReadLine();
+
+                Console.WriteLine("Date Of Birth(example:yyyy-mm-dd)");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Phone(example:+998xxxxxxxxx)");
+                string phone = Console.ReadLine();
+
+                Console.WriteLine("Email(example:example@gmail.com): ");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Experience:(number enter): ");
+                int experience = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Specialty(example:.Net): ");
+                string specialty = Console.ReadLine();
+
+                var result = await teacherService.CreateAsync(new TeacherCreationDto
+                {
+                    FirstName = first_name,
+                    LastName = last_name,
+                    DateOfBirth = date,
+                    Phone = phone,
+                    Email = email,
+                    Experince = experience,
+                    Speciality = specialty
+                });
+
+                if (result.StatusCode == 200)
+                    Console.WriteLine(result.Data.FirstName);
+            }
+            else
+            {
+                Console.WriteLine("Kechirasiz sizni ishga qabul qilolmaymiz!");
+            }
+
         }
 
         public async void Update()
