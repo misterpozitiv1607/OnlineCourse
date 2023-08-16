c61eb5b453106cd50d52eec63f702288639bbf94
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-08-16 13:19:50 +0500

    Response Delete

--- OnlineCourse.Service/Helpers/Response.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OnlineCourse.Service.Helpers;
-
-public class Response<T>
-{
-    public  int StatusCode { get; set; }
-    public string Message { get; set; }
-    public T Data { get; set; }
-}
