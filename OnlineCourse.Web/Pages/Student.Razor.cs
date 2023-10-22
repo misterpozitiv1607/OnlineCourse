7badaf18e2dbdc7177210dff860c7894478e4e80
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-10-22 07:33:49 +0500

    Prosta tak

---
+++ OnlineCourse.Web/Pages/Student.Razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OnlineCourse.Web.Pages
+{
+    public partial class Student
+    {
+        [Parameter]
+        public string Title{ get; set; }
+        [Parameter]
+        public DateOnly Date { get; set; }
+    }
+}
