dca0d6dab9d364678abbfbca623e3b7e21055aae
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2023-10-25 22:38:28 +0500

    Default model remove

--- OnlineCourse.WebMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OnlineCourse.WebMVC.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
